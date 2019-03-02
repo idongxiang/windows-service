4982afc9087673efa73b8628b8ac58a74bb433ac
董翔 <idongxiang@qq.com>
2019-03-02 13:08:53 +0800

    exec

--- src/main/java/io/dongxiang/controller/WindowsController.java
+++ src/main/java/io/dongxiang/controller/WindowsController.java
@@ -1,11 +1,18 @@
 package io.dongxiang.controller;
 
 import eu.bitwalker.useragentutils.UserAgent;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Objects;
 
 /**
@@ -35,11 +42,36 @@ public class WindowsController {
         return "<h1>Rollback Command OK!</h1>";
     }
 
-    private void shutdown(Long seconds) {
-        seconds = Objects.isNull(seconds) || seconds < 300L ? 300L : seconds;
+    @RequestMapping({"/exec"})
+    @ResponseBody
+    public String exec(@RequestHeader("User-Agent") String ua, @RequestParam(name = "command") String command) {
+        logger.info(String.format("Welcome %s !", UserAgent.parseUserAgentString(ua)));
+        String exec = exec(command);
+        logger.info(String.format("Exec Print:\n%s", exec));
+        Parser parser = Parser.builder().build();
+        String input = String.format("```\n%s\n```", exec);
+        Node document = parser.parse(input);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return "<h1>Run Command OK!</h1>" + renderer.render(document);
+    }
+
+    private String exec(String command) {
         try {
             //关机
-            Runtime.getRuntime().exec("shutdown /s /t " + seconds);
+            Process exec = Runtime.getRuntime().exec(command);
+            InputStream is = exec.getInputStream();
+            return IOUtils.toString(is, Charset.forName("GBK"));
+        } catch (Throwable e) {
+            return ExceptionUtils.getStackTrace(e);
+        }
+    }
+
+    private void shutdown(Long seconds) {
+        seconds = Objects.isNull(seconds) || seconds < 300L ? 300L : seconds;
+        String command = "shutdown /s /t " + seconds;
+        try {
+            //关机
+            Runtime.getRuntime().exec(command);
         } catch (IOException ignore) {
         }
     }
